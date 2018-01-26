54f9229720f26560adf82ea473aeead3e0e02ae2
Joakim Unge <ashba@unge.se>
2018-01-26 16:40:53 +0100

    Add styling for reset button and placeholder

--- app.js
+++ app.js
@@ -2,49 +2,184 @@ $(document).ready(() => {
 
 	let idCounter = 0;
 
-	class App {
-		constructor() {
-			this.init();
-			initSort();
+	class State {
+		constructor(board) {
+			const localStorage = window.localStorage;
+			const name = "trello-clone";
+			this.defaultState = {
+		      lists: [
+		        {
+		          id: 0,
+		          title: "Todo",
+		          cards: [
+		            {
+		              id: 4,
+		              title: "Card in todo"
+		            },
+		            {
+		              id: 5,
+		              title: "Card #2 in todo"
+		            }
+		          ]
+		        },
+		        {
+		          id: 1,
+		          title: "In progress",
+		          cards: [
+		            {
+		              id: 6,
+		              title: "Card in progress"
+		            }
+		          ]
+		        },
+		        {
+		          id: 2,
+		          title: "Done",
+		          cards: [
+		            {
+		              id: 7,
+		              title: "Card in done"
+		            }
+		          ]
+		        }
+		      ]
+		    };
+
+		    this.getBoardState();
+
 		}
 
-		init() {
-			this.dialog();
-		}
+		getBoardState() {
+	      return JSON.parse(localStorage.getItem(name)) || this.defaultState;
+	    };
 
-		dialog() {
-			$('#dialog').dialog({
-				autoOpen: false,
-				modal: true,
-				show: { effect: "fadeIn", duration: 800},
-				hide: { effect: "fadeOut", duration: 800}
-			});	
-			$('#dialog__tabs').tabs();	  
-		}
+	    setBoardState(board) {
+	      return localStorage.setItem(name, JSON.stringify((board = board)));
+	    };
 	}
 
-	class List {
-		constructor(title, cards = []) {
-			this.title = title;
-
-			this.cards = [
-				{id: "1", title: "card-1"},
-				{id: "2", title: "card-2"},
-				{id: "3", title: "card-3"},
-			];
-
+	class App {
+		constructor(state) {
+			this.store = state;
+			this.state = state.getBoardState();
+			this.newCard = this.newCard.bind(this);
+			this.newList = this.newList.bind(this);
 			this.init();
 		}
 
 		init() {
 			this.render();
-			// this.renderCards();
+			this.listeners();
+			this.sortable();
+		}
+
+		listeners() {
+			$('.list__new--list').submit(e => {
+				e.preventDefault();
+				this.newList(e);
+				this.sortable();
+  			});
+			$('#reset').on('click', (e) => {
+				this.store.setBoardState(this.store.defaultState);
+				location.reload();
+			});
+		}
+
+		newList(e) {
+			const data = $(e.target).serializeArray();
+			console.log(this.getLatestId(this.state));
+			const id = this.getLatestId(this.state) + 1;
+			const newList = new List(id, data[0].value, [], this);
+			const listObj = {id: id, title: data[0].value, cards: []};
+			let newState = this.state;
+			newState.lists.push(listObj);
+			this.store.setBoardState(newState);
+		}
+
+		newCard(e) {
+			const data = $(e.target).serializeArray();
+			console.log(this.getLatestId(this.state));
+			const id = this.getLatestId(this.state) + 1;
+			const newCard = new Card({
+				id: id,
+				title: data[0].value
+			});
+			const cardObj = {id: id, title: data[0].value};
+			const list = $(e.target).parents().closest('.list').find('ul[data-id]');
+			const listId = list.data('id');
+			let newState = this.state;
+			$.each(newState.lists, (key, value) => {
+				if (newState.lists[key].id === listId) {
+						newState.lists[key].cards.push(cardObj);
+				};
+			})
+			this.store.setBoardState(newState);
+		}
+
+		sortable() {
+			    $(".section")
+				    .sortable({
+				      cursor: "move",
+				      connectWith: ".section",
+				      handle: ".list__header",
+				      helper: "clone",
+				      placeholder: "sortable-placeholder",
+				      revert: true
+				    })
+				    .disableSelection();
+
+			    $(".list__content")
+				    .sortable({
+				    	cursor: "move",
+				    	connectWith: ".list__content",
+				    	helper: "clone",
+				    	placeholder: "list__content__card--placeholder",
+				    	revert: true
+				    })
+				    .disableSelection();
+		}
+
+		render() {
+			$.each(this.state.lists, (( key, value ) => {
+			  const list = new List(value.id, value.title, value.cards, this);
+			}));
+		}
+
+		getLatestId(state) {
+		    let id = -1;
+
+		    state.lists.forEach(list => {
+		      list.cards.forEach(card => {
+		        if (card.id > id) {
+		          id = card.id;
+		        }
+		      });
+		      if(list.id > id) {
+		      	id = list.id;
+		      }
+		    });
+
+		    return id;
+		  }
+
+	}
+
+	class List {
+		constructor(id, title, cards = [], caller) {
+			this.id = id;
+			this.title = title;
+			this.cards = cards;
+			this.caller = caller;
+			this.init();
+		}
+
+		init() {
+			this.render();
 			this.listeners();
 		}
 
 		render() {
-			const cards = this.cards.map(card => new Card(card));
-			console.log(cards);
+			const cards = this.cards.map(card => new Card(card, this.caller));
 			const html =  `
 				<div class="section column">
 					<div class="list">
@@ -55,8 +190,8 @@ $(document).ready(() => {
 								</i>
 							</span>
 						</div>
-						<ul class="list__content">
-							${cards}
+						<ul class="list__content" data-id="${this.id}">
+							${cards.map(card => card.fragment).join("")}						
 						</ul>
 						<ul class="input">
 							<li class="list__new--card">
@@ -71,21 +206,17 @@ $(document).ready(() => {
 			$('.column').last().before(html);
 		}
 
-		renderCards() {
-			$.each(this.cards, (index, value) => {
-				const card = new Card(this.cards[index].title);
-			});
-		}
-
 		listeners() {
 			$('.list__delete').on('click', e => {
 				let target = e.target.closest('.column');
   				target.remove();
+  				e.stopImmediatePropagation();
+  				console.log(this.caller.state);
 			});
 
-			$('.list__new--card').last()
-				.submit(e => {
-					Handlers.newCard(e);
+			$('.list__new--card').last().submit(e => {
+				e.preventDefault();
+				this.caller.newCard(e);
 			});
 		}
 
@@ -97,13 +228,15 @@ $(document).ready(() => {
 	}
 
 	class Card {
-		constructor(card) {
+		constructor(card, caller) {
 			this.card = card;
+			this.target = 'li[data-id=' + this.card.id + ']';
+			this.caller = caller;
 			this.init();
+			this.fragment = this.render(); // slighjtly icky
 		}
 
 		init() {
-			this.render();
 			this.listeners();
 			$('.list__add')
 				.find('input:text')
@@ -127,68 +260,64 @@ $(document).ready(() => {
 
 		listeners() {
 			$('body').on('click', '.card__delete', (e) => {
+				e.stopPropagation();
 				let target = $(e.target).closest('li');
   				target.remove();
 			});
 
-			let target = 'li[data-id=' + this.card.id + ']';
-
-			$('body').on('click', target, (e) => {
-				// $('#dialog').dialog('open');
-				// console.log($(e.target).data('id'));
+			$('body').on('click', this.target, (e) => {
+				e.stopImmediatePropagation();
 				console.log(this);
-			})
-		}
-	}
-
-	const Handlers = {
-		newCard: (e) => {
-			e.preventDefault();
-			const data = $(e.target).serializeArray();
-			const newCard = new Card({
-				id: idCounter,
-				title: data[0].value
+				let modal = new Modal(this.card.title);
 			});
-			idCounter++;
-		},
-		newList: (e) => {
-			e.preventDefault();
-			const data = $(e.target).serializeArray();
-			const newList = new List(data[0].value);
 		}
 	}
 
-	// Initialize sorting on base columns and list
-	const initSort = () => {
-	    $(".section")
-		    .sortable({
-		      cursor: "move",
-		      connectWith: ".section",
-		      handle: ".list__header",
-		      helper: "clone",
-		      placeholder: "sortable-placeholder",
-		      revert: true
-		    })
-		    .disableSelection();
+	class Modal {
+		constructor(title) {
+			this.title = title;
+			this.init();
+			this.modal = $('.modal');
+		}
 
-	    $(".list__content")
-		    .sortable({
-		    	cursor: "move",
-		    	connectWith: ".list__content"
-		    })
-		    .disableSelection();
-  	}
+		init() {
+			this.render();
+			this.listeners();
+		}
 
-  	//Listeners
-  	$('.list__new--card').submit(e => {
-  		Handlers.newCard(e);
-  	});
+		listeners() {
+			$('body').on('click', '.modal__close', (e) => {
+				this.modal.remove();
+			});
+		}
 
-  	$('.list__new--list').submit(e => {
-		Handlers.newList(e);
-		initSort();
-  	});
-	  
-	const app = new App();
+		render() {
+			const modal = `
+				<div class="modal">
+					<div class="modal__content">
+			            <div class="modal__header">
+			                <h1>${this.title}</h1>
+			                <span class="modal__close">
+								<i class="fa fa-times" aria-hidden="true">
+								</i>
+							</span>
+			            </div>
+			            <div class="modal__body">
+			                <p></p>
+			            </div>
+			            <div class="modal__footer">
+			                <p></p>
+			            </div>
+			        </div>
+			        <div class="modal__overlay"></div>
+			    </div>
+	        `
+		    $('body').append(modal);
+		}
+
+	}
+
+	const state = new State();
+	const app = new App(state);
 
 });
